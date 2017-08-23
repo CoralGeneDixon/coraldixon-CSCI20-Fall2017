cdc90bf751ba918269820d18cc2799e83c662716
Coral Dixon <cdixon002@student.butte.edu>
2017-08-23 19:09:07 +0000

    Lab1.1 submission

---
+++ lab11/lab11.cpp
@@ -0,0 +1,10 @@
+Computer loads 10 points onto screen
+Program randomly chooses a number between 1 and 10
+    5 will be the number in this scenario
+User enters number into text box and clicks enter
+    If wrong, decrease original point number by 1
+        Also display number guessed for user to track
+    If correct, display text saying winner.
+        Then restart program with a new random number.
+
+
