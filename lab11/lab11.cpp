c8efa153dd6f957dee71584357e9912bd7a1aac5
Coral Dixon <cdixon002@student.butte.edu>
2017-08-23 19:30:27 +0000

    Lab1.1 submission

--- lab11/lab11.cpp
+++ lab11/lab11.cpp
@@ -1,9 +1,13 @@
 Computer loads 10 points onto screen
 Program randomly chooses a number between 1 and 10
     5 will be the number in this scenario
-User enters number into text box and clicks enter
+User clicks text box
+User types a number into text box between 1 and 10
+User clicks enter
+Program analyzes number and compares it to the number it has chosen
     If wrong, decrease original point number by 1
-        Also display number guessed for user to track
+        Program will also display number guessed for user to track
+        User chooses another number between 1 and 10 that is not the same as any previously chosen numbers
     If correct, display text saying winner.
         Then restart program with a new random number.
 
