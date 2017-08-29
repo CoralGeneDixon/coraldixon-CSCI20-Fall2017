41efaabe98c427c8846a21bfac5f2d70c01d57a3
Coral Dixon <cdixon002@student.butte.edu>
2017-08-29 04:31:07 +0000

    Lab1.2 Commit

---
+++ lab12/lab12.cpp
@@ -0,0 +1,25 @@
+/* Created: Coral Dixon___8/28/17
+ASCII Art that is an image of a dragon___https://usercontent1.hubstatic.com/6066629_f520.jpg*/
+
+ #include <iostream>
+ using namespace std;
+ 
+ int main()
+ {
+     cout << "  <>=======()" << endl;
+     cout << " (/\\___   /|" << "\\\\            ()==========<>_" << endl;
+     cout << "       \\_/ | \\\\          //|\\   ______/ \\)" << endl;
+     cout << "          \\_|  \\\\       // | \\_/" << endl;
+     cout << "             \\|\\/|\\_   //  /\\/" << endl;
+     cout << "              (OO)\\ \\_//  /" << endl;
+     cout << "             //_/\\_\\/ /  |" << endl;
+     cout << "            @@/  |=\\ \\   |" << endl;
+     cout << "                 \\_=\\_\\  |" << endl;
+     cout << "                   \\==\\ \\|\\_" << endl;
+     cout << "                __(\\===\\(  )\\" << endl;
+     cout << "               (((~) __(_/   |" << endl;
+     cout << "                    (((~) \\  /" << endl;
+     cout << "                    ______/ /" << endl;
+     cout << "                    '------'" << endl;
+ }
+
