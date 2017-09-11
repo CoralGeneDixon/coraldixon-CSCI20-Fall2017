7ec52431233d6aad61f41d0b9a77dbe633d79e60
Coral Dixon <cdixon002@student.butte.edu>
2017-09-11 19:20:52 +0000

    Lab1.5 submission

---
+++ lab15/lab15.cpp
@@ -0,0 +1,36 @@
+//This program was created by Coral Dixon for lab 1.5 on 
+//September 11th 2017. The following program allows a user
+//to input a set of 5 random nouns, and 2 adjectives.
+//These words will be generated into a madlib.
+
+ #include <iostream>
+ #include <string>
+ using namespace std;
+ 
+ int main() {
+    string adj_one;
+    string adj_two;
+    string noun_one;
+    string noun_two;
+    string noun_three;
+    string noun_four;
+    string noun_five;
+    
+    cout << "Enter 2 adjectives and 5 nouns in order." << endl; 
+    cout << "No spaces. Tap ENTER to enter each word: " << endl;
+    cin >> adj_one;
+    cin >> adj_two; 
+    cin >> noun_one;
+    cin >> noun_two;
+    cin >> noun_three;
+    cin >> noun_four;
+    cin >> noun_five;
+    
+    cout << "A vacation is when you take a trip to some *" << adj_one << " place with your *" << endl;
+    cout << adj_two << " family. Usually you go to some place that is near a/an *" << noun_one << endl;
+    cout << "or up on a/an *" << noun_two << ". A good vacation place is one where you can ride *" << noun_three << endl;
+    cout << "or play *" << noun_four << " or go hunting for *" << noun_five << endl;
+    
+    
+    return 0;
+}
