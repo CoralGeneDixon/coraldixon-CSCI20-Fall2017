8b6c26c1d163c6cf0f3089df5b674ea737582d26
Coral Dixon <cdixon002@student.butte.edu>
2017-09-27 18:41:43 +0000

    Lab 2.2

---
+++ Lec12/activity.cpp
@@ -0,0 +1,30 @@
+//   Header comments
+//Created By: April Browne
+//Created On: 8/22/2016
+ 
+ 
+ //   Inline comments 
+ //   preprocessor directives tell the program that they are about to run a specific program
+
+ #include <iostream>
+ using namespace std;
+ 
+ 
+ //   Inline comments 
+ //   main function tells program to display hello world and hello coral on 2 separate lines
+
+ int main()
+ {
+     cout<<"Hello "<<"World!"<<endl;
+     cout<<"Hello "<<"Coral!"<<endl;
+ }
+ 
+ 
+ //   Inline comments
+ 
+ //Identify the following parts of the program:
+ //   preprocessor directives
+ //   main function
+ //   comments
+ //update it so that it is human readable
+ //add a second output line that says "Hello YourName!"
